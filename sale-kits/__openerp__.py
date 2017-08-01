e52959414567729eec9cebb33fb73791c0ca21cf
sergson <serg_terihov@mail.ru>
2017-08-01 19:41:28 +0700

    Description

--- sale-kits/__openerp__.py
+++ sale-kits/__openerp__.py
@@ -35,7 +35,6 @@ Module give a possibility to create any kind of sale order lines kits and save i
 	'price': '10',
 	'currency': 'EUR',
     'depends': ['sale'],
-	'images': ['images/sale-kits.pgn'],
     'data': [
         'security/ir.model.access.csv',
         'wizard/sale_kits_wizard_view.xml',
