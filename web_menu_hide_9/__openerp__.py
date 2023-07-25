142a72f033124cb722e33852f31e07be12978667
sergson <serg_terihov@mail.ru>
2023-07-25 22:36:16 +0700

    Add files via upload

---
+++ web_menu_hide_9/__openerp__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': 'Web Menu Hide/Show',
+    'category': 'Theme',
+    'version': '1.0',
+    'sequence': 3,
+    'summary': 'Web Backend To Blue',
+    'website': 'http://boscosoft.com/',
+    'description': 'Web Menu Hide/Show',
+    'author': 'BoscoSoft',
+    'depends': [],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'bootstrap': True, # load translations for login screen
+    'application': True,
+}
