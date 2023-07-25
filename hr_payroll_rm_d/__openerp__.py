c2dd8d1241fe314499a54d4ffe00f2669a19ffa1
sergson <serg_terihov@mail.ru>
2023-07-25 21:52:46 +0700

    Add files via upload

---
+++ hr_payroll_rm_d/__openerp__.py
@@ -0,0 +1,29 @@
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Payroll Modifications for Russian accounting data',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'sequence': 38,
+    'description': """
+Payroll Modifications for Russian accounting data.
+=============================================
+Salary structure data adding
+    """,
+    'website': 'https://www.odoo.com/page/employees',
+    'author': 'Serg Terihov'
+    'depends': [
+        'hr_payroll',
+        'hr_payroll_account',
+        'hr_payroll_rm',
+    ],
+    'data': [
+        'hr_payroll_data.xml'
+    ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
